2d74aca511a2dacee1aebc92dbbabb251345a2d3
yanagi-ori <yanagiori@gmail.com>
2019-01-13 20:25:38 +0300

    Revert "Revert "Revert "Bogatyrev"""
    This reverts commit c3a65d2780124b5544c840286c2687ba1834f504.

--- 1st_semester/Подготовка/Бесконечный ряд.py
+++
@@ -1,8 +0,0 @@
-eps = float(input('input precision: '))
-x = float(input('input number x: '))
-n = 0; t = 1; y = 1
-while abs(t) > eps:
-    n += 1
-    t *= x/n
-    y += t
-print('x =', x, '   y =', y)
